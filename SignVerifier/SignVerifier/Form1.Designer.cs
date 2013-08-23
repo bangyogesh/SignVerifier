ab43b2bb0c6c079b7d3bacee73eab350ec8381bc
yogonweb <yogonweb@gmail.com>
2013-08-23 19:13:59 +0530

    Welcome caption edited

--- SignVerifier/SignVerifier/Form1.Designer.cs
+++ SignVerifier/SignVerifier/Form1.Designer.cs
@@ -36,7 +36,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
             this.Name = "Form1";
-            this.Text = "Welcome";
+            this.Text = "Welcome.";
             this.ResumeLayout(false);
 
         }
